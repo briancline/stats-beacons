83c2e19405d862c05d887c11821788b6ff52c1ce
Brian Cline <bcline@softlayer.com>
2013-01-30 21:59:48 -0600

    Add a log tailing poller to emit raw log lines in the description field to Riemann; intended to allow for basic matching/processing within a stream

---
+++ log-tailer.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import time
+import subprocess
+from beacon import PollingBeacon
+
+
+class LogTailerBeacon(PollingBeacon):
+    name = 'log-tailer'
+
+    def beacon_init(self):
+        self.service = self.get_val('service_name')
+        self.filename = self.get_val('file_name')
+        self.proc = subprocess.Popen(['tail', '-F', self.filename],
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE)
+
+    def poll(self):
+        while True:
+            line = self.proc.stdout.readline()
+            print line
+            self.send(line)
+
+    def send(self, line):
+        print({'host': self.get_hostname(),
+               'service': self.service_name,
+               'state': "ok",
+               'time': time.time(),
+               'metric': 0,
+               'description': line})
+        self.riemann.send({'host': self.get_hostname(),
+                           'service': self.service_name,
+                           'state': "ok",
+                           'time': time.time(),
+                           'metric': 0,
+                           'description': line})
+
+
+if __name__ == '__main__':
+    beacon = LogTailerBeacon()
+    beacon.run()
